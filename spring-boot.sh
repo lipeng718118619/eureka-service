fd953a72950c4d267e88d2f228d363d7cbb13d6d
lipeng <lipengab@chanjet.com>
2018-04-16 09:53:39 +0800

    add eureka-client eureka-service

--- spring-boot.sh
+++ spring-boot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 APP_NAME=$1
-nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME > /dev/null 2>&1 &
+nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME --spring.profiles.active=alpha > /dev/null 2>&1 &
+nohup java $JAVA_OPTS -Dfile.encoding=UTF-8 -Xms64m -Xmx128m -jar $APP_NAME --spring.profiles.active=beta > /dev/null 2>&1 &
+
 
 
